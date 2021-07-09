6aa8644a67aafb7029678abaddbf45da3de7770d
therikb31 <therikb31@gmail.com>
2021-07-09 18:22:56 +0530

    Create Account Transaction

--- ATM.c
+++ ATM.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
-void myimport();
+void import();
 void createAccount();
 void writer();
+void intToStr(char*,int);
 struct data
 {
     int accountNumber;
@@ -16,15 +17,15 @@ struct data acc[100];
 int dataCount=0;
 void main()
 {
-    myimport();
+    import();
     createAccount();
     writer();
 }
-void myimport()//Import data from Account Details
+void import()//Import data from Account Details
 {
     FILE *fp;
     fp=fopen("accDet.txt","r");
-    int i=0,j;
+    int i=0;
     while(!feof(fp))
     {
         fscanf(fp,"%d %s %s %d %d",&acc[i].accountNumber,acc[i].firstName,acc[i].lastName,&acc[i].pin,&acc[i].balance);
@@ -33,10 +34,12 @@ void myimport()//Import data from Account Details
     dataCount=i;
     fclose(fp);
 }
-void createAccount()
+void createAccount()//Create Account and Store it in structure acc 
 {
+    FILE* fp;
     int accno;
-    accno=acc[dataCount-1].accountNumber+1;
+    char accstr[10]="",fileName[30]="./Transaction/";
+    accno=acc[dataCount-1].accountNumber+1;//100009+1=100010
     acc[dataCount].accountNumber=accno;
     printf("Enter your First Name :");
     scanf("%s", acc[dataCount].firstName);
@@ -46,10 +49,17 @@ void createAccount()
     scanf("%d",&acc[dataCount].pin);
     printf("Enter the Opening Balance:");
     scanf("%d",&acc[dataCount].balance);
-    printf("Your account number is %d",accno);
+    printf("Your account number is %d\n",accno);
+    intToStr(&accstr[0],accno);
+    strcat(fileName,accstr);
+    strcat(fileName,".txt");
+    fp=fopen(fileName,"w");
+    fprintf(fp,"0\t+%d\t%d\n",acc[dataCount].balance,acc[dataCount].balance);
     dataCount++;
+    fclose(fp);
+    
 }
-void writer()
+void writer()//Writes all the changes made to Acount Details
 {
     FILE *fp;
     fp=fopen("accDet.txt","w");
@@ -63,4 +73,20 @@ void writer()
         }
     }
     fclose(fp);
+}
+void intToStr(char* st,int n)
+{
+    int arr[6],r,c=0,d=0,i;
+    while(n>0)
+    {
+        r=n%10;
+        n=n/10;
+        arr[c]=r;
+        c++;
+    }//arr=6,5,4,3,2,1
+    for(i=c-1;i>=0;i--)//i=5
+    {//0 -> 48 to 57
+        st[d]=(char)arr[i]+48;
+        d++;
+    }
 }
